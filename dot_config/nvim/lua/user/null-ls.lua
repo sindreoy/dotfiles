7310e94c320c6e101189dac5cb6e412d70dd208f
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2023-07-19 16:22:42 +0200

    Fix formatter and add a disabled plugins file

--- dot_config/nvim/lua/user/null-ls.lua
+++ dot_config/nvim/lua/user/null-ls.lua
@@ -17,6 +17,18 @@ function M.config()
   local formatting = null_ls.builtins.formatting
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   local diagnostics = null_ls.builtins.diagnostics
+
+  local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format {
+      filter = function(client)
+        -- apply whatever logic you want (in this example, we'll only use null-ls)
+        return client.name == "null-ls"
+      end,
+      bufnr = bufnr,
+    }
+  end
+
+  -- if you want to set up formatting on save, you can use this as a callback
   local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
   -- https://github.com/prettier-solidity/prettier-plugin-solidity
@@ -32,15 +44,15 @@ function M.config()
     },
 
     -- See also https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+    -- you can reuse a shared lspconfig on_attach callback here
     on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      if client.supports_method "textDocument/formatting" then
+        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
         vim.api.nvim_create_autocmd("BufWritePre", {
           group = augroup,
           buffer = bufnr,
           callback = function()
-            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-            vim.lsp.buf.format()
+            lsp_formatting(bufnr)
           end,
         })
       end
