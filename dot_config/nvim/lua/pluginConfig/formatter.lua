eaf525377476954ac053e6876bd3b77ffa3922ff
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2023-09-20 15:53:48 +0200

    Use eslint_d to make prettier forward to eslint --fix

--- dot_config/nvim/lua/pluginConfig/formatter.lua
+++ dot_config/nvim/lua/pluginConfig/formatter.lua
@@ -1,35 +1,35 @@
-require("formatter").setup({
-	-- Enable or disable logging
-	logging = true,
-	-- Set the log level
-	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
-	filetype = {
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
 
-		-- Lua
-		lua = { require("formatter.filetypes.lua").stylua },
+    -- Lua
+    lua = { require("formatter.filetypes.lua").stylua },
 
-		-- Python
-		python = {
-			require("formatter.filetypes.python").black,
-			require("formatter.filetypes.python").isort,
-		},
+    -- Python
+    python = {
+      require("formatter.filetypes.python").black,
+      require("formatter.filetypes.python").isort,
+    },
 
-		typescript = {
-			require("formatter.filetypes.typescript").prettier,
-		},
+    typescript = {
+      require("formatter.filetypes.typescript").eslint_d,
+    },
 
-		typescriptreact = {
-			require("formatter.filetypes.typescriptreact").prettier,
-		},
+    typescriptreact = {
+      require("formatter.filetypes.typescript").eslint_d,
+    },
 
-		markdown = {
-			require("formatter.filetypes.markdown").prettier,
-		},
+    markdown = {
+      require("formatter.filetypes.markdown").eslint_d,
+    },
 
-		["*"] = {
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-			require("formatter.filetypes.any").prettier,
-		},
-	},
-})
+    ["*"] = {
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+      require("formatter.filetypes.any").eslint_d,
+    },
+  },
+}
